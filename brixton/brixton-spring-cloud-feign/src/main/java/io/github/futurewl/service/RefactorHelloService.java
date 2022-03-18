74f3b53450d02953c406b0b2663d3cd73f19130e
Future. 魏来 <624263934@qq.com>
2022-03-18 15:27:37 +0800

    更新：调整为 edgware 版本

--- brixton/brixton-spring-cloud-feign/src/main/java/io/github/futurewl/service/RefactorHelloService.java
+++
@@ -1,9 +0,0 @@
-package io.github.futurewl.service;
-
-
-import io.github.futurewl.service.didi.HelloService;
-import org.springframework.cloud.netflix.feign.FeignClient;
-
-@FeignClient(value = "hello-service")
-public interface RefactorHelloService extends HelloService {
-}
