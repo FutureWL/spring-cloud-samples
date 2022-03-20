a69854e73b1b8592c8e04f7b7a641433dc0c949f
Future. 魏来 <624263934@qq.com>
2022-03-20 15:18:19 +0800

    更新：修改项目路径格式

---
+++ micro/micro-config-client-bus/src/main/java/io/github/futurewl/FinchleyWeatherApplicationMicroWeatherConfigServerBus.java
@@ -0,0 +1,12 @@
+package io.github.futurewl;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class FinchleyWeatherApplicationMicroWeatherConfigServerBus {
+    public static void main(String[] args) {
+
+    }
+}
